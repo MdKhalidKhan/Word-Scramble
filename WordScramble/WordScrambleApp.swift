3cb39d1fb7a72c592e25a420d3815b4b83736b5f
Raj <rajkhulna@outlook.com>
2022-08-17 17:31:34 +0600

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Khalid Khan on 17/8/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
